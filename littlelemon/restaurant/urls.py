6630ee6d585dc4491f2c87e189c918a54bb7c7fb
Biruk Wubeshet <birukethg@gmail.com>
2024-01-30 21:15:56 +0300

    Defined menu and booking API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'booking/tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
+
+urlpatterns += router.urls
