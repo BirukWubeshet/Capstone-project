2b2256a95ce60080d06b03c121125b96d025c5a6
Biruk Wubeshet <birukethg@gmail.com>
2024-02-02 19:43:50 +0300

    added unit tests for model and view

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
-# Create your tests here.
+from .models import Menu
+from .serializers import MenuSerializer
+from .views import MenuItemView
+
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title='IceCream', price=80, inventory=100)
+        self.assertEqual(item.__str__(), 'IceCream : 80')
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        
+        Menu.objects.create(title='IceCream', price=80, inventory=100)
+        Menu.objects.create(title='Salad', price=100, inventory=00)
+    
+    def test_getall(self):
+        request = self.factory.get('/restaurant/menu/')
+        
+        menuitems = Menu.objects.all()
+        serialized_menuitems = MenuSerializer(menuitems, many=True)
+        
+        response = MenuItemView.as_view()(request)
+        
+        self.assertEqual(response.data, serialized_menuitems.data)
