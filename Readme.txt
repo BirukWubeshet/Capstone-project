6578ec43de202382fe68665b302bd8b8a173c59c
Biruk Wubeshet <birukethg@gmail.com>
2024-02-03 20:10:08 +0300

    Added Readme text file containing api endpoints

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+restaurant/menu
+restaurant/booking/tables/
+restaurant/menu/<int:pk>
+auth/token/login
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,13 +4,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'booking/tables', views.BookingViewSet)
+router.register(r'restaurant/booking/tables', views.BookingViewSet)
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('', views.index, name='index'),
-    path('menu', views.MenuItemView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('restaurant/menu', views.MenuItemView.as_view()),
+    path('restaurant/menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
 
 urlpatterns += router.urls
